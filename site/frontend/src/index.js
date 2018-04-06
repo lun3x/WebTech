2ce0f849ece960843d1bfe4ff9e2e61296b6b173
Ahmer Butt <ab15015@bristol.ac.uk>
2018-04-06 12:16:56 +0100

    added react with babel, webpack
    created new frontend folder for our new frontend
    setup frontend with npm
    setup webpack to bundle dev stuff from src/* to dist/* (minifying,
    etc..)
    added npm scripts to build for both dev/production modes
    setup babel to compile es6/jsx and plugin with webpack
    setup react and plugin with webpack
    setup html loader and plugin with webpack
    setup css loader and plugin with webpack

---
+++ site/frontend/src/index.js
@@ -0,0 +1,11 @@
+import style from "./main.css";
+import App from "./App";
+
+console.log(`I'm a silly entry point`);i
+
+const arr = [1,2,3];
+
+const jsES6 = () => console.log(...arr);
+
+window.jsES6 = jsES6;
+
