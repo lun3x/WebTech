098ffcd5e340df2e958c94eb4493010e45a107d5
Ahmer Butt <ab15015@bristol.ac.uk>
2018-04-09 14:17:07 +0100

    use migrations instead of setup.sql
    add db-migrate to project
    create multiple migrations instead of setup.sql script

---
+++ site/migrations/20180409114401-addDefaultCupboardIdToUsers.js
@@ -0,0 +1,43 @@
+let dbm;
+let type;
+let seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = (options, seedLink) => {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+};
+
+exports.up = (db, callback) => {
+    db.addColumn('Users', 'default_cupboard_id', {
+        type: 'int',
+        notNull: true,
+        unsigned: true,
+        foreignKey: {
+            name: 'FK_DefaultCupboard',
+            table: 'Cupboards',
+            rules: {
+                onDelete: 'CASCADE',
+                onUpdate: 'RESTRICT'
+            },
+            mapping: {
+                default_cupboard_id: 'id'
+            }
+        }
+    }, callback);
+};
+
+exports.down = (db, callback) => {
+    db.removeForeignKey('Users', 'FK_DefaultCupboard', (err) => {
+        if (err) callback(err);
+        db.removeColumn('Users', 'default_cupboard_id', callback);
+    });
+};
+
+exports._meta = {
+    version: 1
+};
