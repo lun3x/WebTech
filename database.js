f70c7ae9306196aae315008e9e566972476a708a
Ahmer Butt <ab15015@bristol.ac.uk>
2018-05-18 23:22:02 +0100

    setup db to connect to jaws or localj

--- database.js
+++ database.js
@@ -2,11 +2,30 @@ const mysql = require('mysql');
 
 
 // Use a pool
-exports.con = mysql.createPool({
-    connectionLimit: 100,
-    host: process.env.JAWSDB_URL || 'localhost',
-    user: 'root',
-    password: 'pass',
-    database: 'mydb2',
-});
+// exports.con = mysql.createPool({
+//     connectionLimit: 100,
+//     host: process.env.JAWSDB_URL || 'localhost',
+//     user: 'root',
+//     password: 'pass',
+//     database: 'mydb2',
+// });
 
+
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    //local host
+    connection = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: 'pass',
+        database: 'mydb2',
+    });
+}
+
+exports.con = connection;
